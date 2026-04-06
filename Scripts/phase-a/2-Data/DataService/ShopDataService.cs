ca5795ce5fa77a0d3f3e4ad8652c19732912b68e
an-alch3mist <kaeya000@gmail.com>
2026-04-07 01:01:24 +0600

    inProgress

--- Scripts/phase-a/2-Data/DataService/ShopDataService.cs
+++ Scripts/phase-a/2-Data/DataService/ShopDataService.cs
@@ -7,6 +7,7 @@ using SPACE_UTIL;
 
 /// <summary>
 /// plain C# data collection service.
+/// doesnt alter anything but internally
 /// </summary>
 public class ShopCategoryItemService
 {
@@ -16,23 +17,23 @@ public class ShopCategoryItemService
 
 	#region private API
 	List<SO_ShopCategory> CATEGORY;
-	Dictionary<SO_ShopCategory, List<WShopItem>> MAP__CATEGORY__WITEM_LIST;
+	Dictionary<SO_ShopCategory, List<WShopItem>> DOC__CATEGORY__WITEM_LIST;
 	#endregion
 
 	#region public API
 	public void Build(List<SO_ShopCategory> CATEGORY)
 	{
 		this.CATEGORY = CATEGORY;
-		MAP__CATEGORY__WITEM_LIST = new Dictionary<SO_ShopCategory, List<WShopItem>>();
+		DOC__CATEGORY__WITEM_LIST = new Dictionary<SO_ShopCategory, List<WShopItem>>();
 		foreach (var category in CATEGORY)
-			MAP__CATEGORY__WITEM_LIST[category] = category.ITEM_DEF.map(def => new WShopItem(def)).ToList();
+			DOC__CATEGORY__WITEM_LIST[category] = category.ITEM_DEF.map(def => new WShopItem(def)).ToList();
 	}
 
 	public List<SO_ShopCategory> GetCategories() => CATEGORY;
-	public List<WShopItem> GetWItems(SO_ShopCategory category) => MAP__CATEGORY__WITEM_LIST[category];
+	public List<WShopItem> GetWItems(SO_ShopCategory category) => DOC__CATEGORY__WITEM_LIST[category];
 	public bool IsAllLocked(SO_ShopCategory category)
 	{
-		foreach (var wItem in MAP__CATEGORY__WITEM_LIST[category])
+		foreach (var wItem in DOC__CATEGORY__WITEM_LIST[category])
 			if (wItem.isLockedCurr == false)
 				return false;
 		return true;
@@ -40,7 +41,7 @@ public class ShopCategoryItemService
 	// LOG
 	public string GetSnapShot()
 	{
-		return PhaseALOG.
+		return $"{PhaseALOG.LIST_CATEGORY__TO__JSON(CATEGORY)}\n\n{PhaseALOG.DOC_CATEGORY__TO__ITEM(DOC__CATEGORY__WITEM_LIST)}";
 	}
 	#endregion
 }
